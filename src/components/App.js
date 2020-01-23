ca32f6ea8d0135318c243ca8a70cdda2d36e6d93
Mehdi Arbabi <mehdi_arbabi@yahoo.com>
2020-01-23 18:41:18 +0330

    Before Adding Redux

--- src/components/App.js
+++ src/components/App.js
@@ -8,51 +8,51 @@ import BasketContext from './BasketContext';
 import Content from './Content';
 
 export default class App extends Component {
-  state = {
-    basketProducts: [],
-    basketCount: 0,
-    showSideMenu: false
-  }
-  componentDidMount() {
-    this.loadBasket();
-  }
-  toggleSideMenuHandler = () => {
-    this.setState(state => ({ showSideMenu: !state.showSideMenu }))
-  }
-  render() {
-    return (
-      <Router>
-        <BasketContext.Provider
-          value={{
-            basketProducts: this.state.basketProducts,
-            basketCount: this.state.basketCount,
-            addToBasket: this.addToBasket,
-            removeFromBasket: this.removeFromBasket,
-          }}
-        >
-          {/* <div className="App"> */}
-            <Header toggleSideMenuHandler={this.toggleSideMenuHandler} />
-            <SideMenu show={this.state.showSideMenu} />
-            <Content />
-          {/* </div> */}
-        </BasketContext.Provider>
-      </Router>
-    );
-  }
-  addToBasket = ({ id, name, price }) => {
-    localStorage.setItem(id, JSON.stringify({ id, name, price }))
-    this.loadBasket();
-  }
-  loadBasket = () => {
-    const basketProducts = Object.keys(localStorage).sort().map(productId => JSON.parse(localStorage.getItem(productId)));
-    this.setState({ basketProducts: basketProducts, basketCount: basketProducts.length });
-  }
-  removeFromBasket = (productId) => {
-    console.log('hello', productId);
+	state = {
+		basketProducts: [],
+		basketCount: 0,
+		showSideMenu: false
+	}
+	componentDidMount() {
+		this.loadBasket();
+	}
+	toggleSideMenuHandler = () => {
+		this.setState(state => ({ showSideMenu: !state.showSideMenu }))
+	}
+	render() {
+		return (
+			<Router>
+				<BasketContext.Provider
+					value={{
+						basketProducts: this.state.basketProducts,
+						basketCount: this.state.basketCount,
+						addToBasket: this.addToBasket,
+						removeFromBasket: this.removeFromBasket,
+					}}
+				>
+					{/* <div className="App"> */}
+					<Header toggleSideMenuHandler={this.toggleSideMenuHandler} />
+					<SideMenu show={this.state.showSideMenu} />
+					<Content />
+					{/* </div> */}
+				</BasketContext.Provider>
+			</Router>
+		);
+	}
+	addToBasket = ({ id, name, price }) => {
+		localStorage.setItem(id, JSON.stringify({ id, name, price }))
+		this.loadBasket();
+	}
+	loadBasket = () => {
+		const basketProducts = Object.keys(localStorage).sort().map(productId => JSON.parse(localStorage.getItem(productId)));
+		this.setState({ basketProducts: basketProducts, basketCount: basketProducts.length });
+	}
+	removeFromBasket = (productId) => {
+		console.log('hello', productId);
 
-    localStorage.removeItem(productId);
-    this.loadBasket();
-  }
+		localStorage.removeItem(productId);
+		this.loadBasket();
+	}
 
 }
 
