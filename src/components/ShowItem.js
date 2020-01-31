284d3efb59aff7bbce47f5f728ab546fb6990596
Mehdi Arbabi <mehdi_arbabi@yahoo.com>
2020-02-01 03:25:47 +0330

    Adding remove from basket function to ShowItem.js

--- src/components/ShowItem.js
+++ src/components/ShowItem.js
@@ -7,34 +7,46 @@ import * as actionTypes from '../store/actions'
 
 function ShowItem(props) {
     //const basketContext = useContext(BasketContext)
-    const product = items.find(product => product.id === props.id)
+    const product = items.find(product => product.id === props.id);
+    const isAddedToBasket = props.basketProducts.some(product => product.id === props.id);
     let img;
     try {
         img = require(`../assets/images/items/${product.id}.jpg`);
     } catch {
         img = noImage;
     }
+    function addRemoveButtonHandler() {
+        if (isAddedToBasket)
+            props.removeProductsHandler({ ...product })
+        else
+            props.addProductsHandler({ ...product })
+    };
     return (
         <div className="ShowItem container">
             <button
-                className="btn btn-primary float-left"
-                onClick={() => { props.addProductsHandler({ ...product }) }}
+                className={"btn float-left " + (isAddedToBasket ? "btn-danger" : "btn-primary")}
+                onClick={addRemoveButtonHandler}
             >
-
-                اضافه به سبد خرید
-                </button>
+                {isAddedToBasket ? "حذف از سبدخرید" : "اضافه به سبدخرید"}
+            </button>
             <h2>{product.name}</h2>
             <h4>{product.price} تومان</h4>
-            <img src={img} alt=""/>
+            <img src={img} alt="" />
         </div>
     );
 
 }
 
+const mapStateToProps = state => {
+    return {
+        basketProducts: state.basketProducts
+    }
+};
 const mapDispatchToProps = dispatch => {
     return {
-        addProductsHandler: ({ id, name, price }) => dispatch({ type: actionTypes.ADD_PRODUCT, id, name, price })
+        addProductsHandler: ({ id, name, price }) => dispatch({ type: actionTypes.ADD_PRODUCT, id, name, price }),
+        removeProductsHandler: ({ id }) => dispatch({ type: actionTypes.REMOVE_PRODUCT, id })
     }
 };
 
-export default connect(null, mapDispatchToProps)(ShowItem);
+export default connect(mapStateToProps, mapDispatchToProps)(ShowItem);
